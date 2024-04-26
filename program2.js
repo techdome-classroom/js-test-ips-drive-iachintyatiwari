b1eb9343dce8f445a06ba0f333fe38cba33d2ecf
iachintyatiwari <143349438+iachintyatiwari@users.noreply.github.com>
2024-04-26 13:48:11 +0530

    Update program2.js
    program 2

--- program2.js
+++ program2.js
@@ -1,7 +1,19 @@
 function longestSubstring(s) {
+    let longestLength = 0;
+    let startOfSubstring = 0;
+    const characterMap = {};
 
+    for (let i = 0; i < s.length; i++) {
+        const currentCharacter = s[i];
+        if (characterMap[currentCharacter] >= startOfSubstring) {
+            startOfSubstring = characterMap[currentCharacter] + 1;
+        }
+        characterMap[currentCharacter] = i;
+        const currentSubstringLength = i - startOfSubstring + 1;
+        longestLength = Math.max(longestLength, currentSubstringLength);
+    }
 
-    // Implementation of longestSubstring function
+    return longestLength;
 }
 
 module.exports = { longestSubstring };
