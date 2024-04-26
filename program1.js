b59e22bfbaf9ef1952f2bb435a16b1bd36e35976
iachintyatiwari <143349438+iachintyatiwari@users.noreply.github.com>
2024-04-26 13:53:36 +0530

    mrogram 2

--- program1.js
+++ program1.js
@@ -1,18 +1,19 @@
 function smallestMissingPositiveInteger(nums) {
-  const positiveNums = nums.filter(num => num > 0);
-  positiveNums.sort((a, b) => a - b);
-
-  let smallestMissing = 1;
-  for (let i = 0; i < positiveNums.length; i++) {
-      if (positiveNums[i] === smallestMissing) {
-          smallestMissing++;
-      } else if (positiveNums[i] > smallestMissing) {
-          return smallestMissing;
-      }
+    const positiveNums = nums.filter(num => num > 0);
+    positiveNums.sort((a, b) => a - b);
+  
+    let smallestMissing = 1;
+    for (let i = 0; i < positiveNums.length; i++) {
+        if (positiveNums[i] === smallestMissing) {
+            smallestMissing++;
+        } else if (positiveNums[i] > smallestMissing) {
+            return smallestMissing;
+        }
+    }
+  
+    return smallestMissing;
   }
-
-  return smallestMissing;
-}
-
-module.exports = smallestMissingPositiveInteger;
-
+  
+  module.exports = smallestMissingPositiveInteger;
+  
+  
