9b3306e392db6040424ac2052b621e0b7089fdb4
iachintyatiwari <143349438+iachintyatiwari@users.noreply.github.com>
2024-04-26 13:56:15 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -1,19 +1,18 @@
 function smallestMissingPositiveInteger(nums) {
-    const positiveNums = nums.filter(num => num > 0);
-    positiveNums.sort((a, b) => a - b);
-  
-    let smallestMissing = 1;
-    for (let i = 0; i < positiveNums.length; i++) {
-        if (positiveNums[i] === smallestMissing) {
-            smallestMissing++;
-        } else if (positiveNums[i] > smallestMissing) {
-            return smallestMissing;
-        }
-    }
-  
-    return smallestMissing;
+  const positiveNums = nums.filter(num => num > 0);
+  positiveNums.sort((a, b) => a - b);
+
+  let smallestMissing = 1;
+  for (let i = 0; i < positiveNums.length; i++) {
+      if (positiveNums[i] === smallestMissing) {
+          smallestMissing++;
+      } else if (positiveNums[i] > smallestMissing) {
+          return smallestMissing;
+      }
   }
-  
-  module.exports = smallestMissingPositiveInteger;
-  
-  
+
+  return smallestMissing;
+}
+
+module.exports = smallestMissingPositiveInteger;
+
